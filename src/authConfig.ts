b6554b80d38350dcb8d002595d8308cdbde6c358
eazhumalaikaliyaperumal <eazhumalai.kaliyaperumal@ariyalsoft.in>
2025-04-10 14:46:43 +0100

    #sso

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,8 +1,9 @@
 export const msalConfig = {
   auth: {
-    clientId: "YOUR_CLIENT_ID",
-    authority: "https://login.microsoftonline.com/YOUR_TENANT_ID",
-    redirectUri: "http://localhost:3000",
+    clientId: "91195dc8-f148-4856-9d9f-3fa0da96fc51",
+    authority:
+      "https://login.microsoftonline.com/8ddacd67-cb2c-429e-8a54-89d6c1f47b8b",
+    redirectUri: "https://sprightly-pika-c980bd.netlify.app/",
   },
   cache: {
     cacheLocation: "localStorage", // or "sessionStorage"
