f8c7f652482764a146fb1fd4c475c4a0e9ca2da8
eazhumalaikaliyaperumal <eazhumalai.kaliyaperumal@ariyalsoft.in>
2025-04-13 22:56:19 +0100

    #14

--- src/main.tsx
+++ src/main.tsx
@@ -7,22 +7,26 @@ import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from "./authConfig.ts";
 
 const msalInstance = new PublicClientApplication(msalConfig);
-// Handle redirect promise
-msalInstance
-  .handleRedirectPromise()
-  .then((response) => {
-    if (response) {
-      console.log("Login successful:", response);
-    }
-  })
-  .catch((error) => {
-    console.error("Error during redirect handling:", error);
-  });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
-  </StrictMode>
-);
+// Initialize the MSAL instance and handle redirect promise
+msalInstance.initialize().then(() => {
+  msalInstance
+    .handleRedirectPromise()
+    .then((response) => {
+      if (response) {
+        console.log("Login successful:", response);
+      }
+    })
+    .catch((error) => {
+      console.error("Error during redirect handling:", error);
+    });
+
+  // Render the React app after MSAL initialization
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </StrictMode>
+  );
+});
