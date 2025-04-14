18e10cac59f8fadd8402e543e5e2a71eaa8e34c5
eazhumalaikaliyaperumal <eazhumalai.kaliyaperumal@ariyalsoft.in>
2025-04-14 08:11:33 +0100

    #27

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useMsal } from "@azure/msal-react";
 import useNavigateBasedOnApi from "./useNavigateBasedOnApi";
 
@@ -7,7 +7,11 @@ import Header from "./Header";
 
 const Layout: React.FC = () => {
   const { accounts } = useMsal();
-  const username = accounts.length > 0 ? accounts[0].username : "Guest";
+  const username = useMemo(
+    () => (accounts.length > 0 ? accounts[0].username : "Guest"),
+    [accounts]
+  );
+
   console.log("username layout comp", username);
 
   // Use the custom hook
--- src/RealEstate.tsx
+++ src/RealEstate.tsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 const RealEstate = () => {
-  return <div>Real Estate</div>;
+  const [num1, setNum1] = useState<string>("0");
+  const [num2, setNum2] = useState<string>("0");
+  const [result, setResult] = useState<number>(0);
+
+  const handleAddition = () => {
+    const parsedNum1 = parseFloat(num1) || 0;
+    const parsedNum2 = parseFloat(num2) || 0;
+    setResult(parsedNum1 + parsedNum2);
+  };
+  return (
+    <div>
+      <h1>Math Calculator</h1>
+      <div>
+        <input
+          type="number"
+          value={num1}
+          onChange={(e) => setNum1(e.target.value)}
+          placeholder="Enter first number"
+        />
+        <input
+          type="number"
+          value={num2}
+          onChange={(e) => setNum2(e.target.value)}
+          placeholder="Enter second number"
+        />
+        <button onClick={handleAddition}>Add</button>
+      </div>
+      <h2>Result: {result}</h2>
+    </div>
+  );
 };
 
 export default RealEstate;
